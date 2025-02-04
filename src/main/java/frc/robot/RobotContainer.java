140721e5c07239d3d8c249fa9beded7bcc184519
Karma770 <pmkeblbe@harrisonschools.com>
2025-02-04 15:34:47 -0500

    Limelight pid controlling

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,11 +17,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-
-
-
+import frc.robot.commands.AlignCommand;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.VisionSubsystem;
 
 
 
@@ -29,6 +28,8 @@ public class RobotContainer {
     
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+
+    private final VisionSubsystem m_Vision = new VisionSubsystem();
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
@@ -78,6 +79,7 @@ public class RobotContainer {
         joystick.pov(180).whileTrue(drivetrain.applyRequest(() ->
             forwardStraight.withVelocityX(-0.5).withVelocityY(0))
         );
+        joystick.x().whileTrue(new AlignCommand(drivetrain, m_Vision));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
--- src/main/java/frc/robot/constants.java
+++ src/main/java/frc/robot/constants.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Inches;
+
 public class constants {
     
     public static class CANConfig {
@@ -19,5 +24,14 @@ public class constants {
         public static final double CORAL_INTAKE_SPEED = 0.5;
       }
 
+        public static class VisionConstants {
+    public static final String LIMELIGHT_NAME = "";
+    public static final Distance LIMELIGHT_LENS_HEIGHT = Distance.ofBaseUnits(8, Inches);
+    public static final Angle LIMELIGHT_ANGLE = Angle.ofBaseUnits(0, Degrees);
+
+    public static final Distance REEF_APRILTAG_HEIGHT = Distance.ofBaseUnits(6.875, Inches);
+    public static final Distance PROCCESSOR_APRILTAG_HEIGHT = Distance.ofBaseUnits(45.875, Inches);
+    public static final Distance CORAL_APRILTAG_HEIGHT = Distance.ofBaseUnits(53.25, Inches);
+  }
 
     }
